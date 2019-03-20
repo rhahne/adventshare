ea5769ce1fc2ab09e49b44a2848a75ef2b3179e4
mnib2xu <b.r.wakker@gmail.com>
2019-03-20 09:53:52 +0100

    search page

--- client/src/components/general/Search.js
+++ client/src/components/general/Search.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 
+import {
+  Field,
+  Control,
+  Label,
+  Select,
+} from 'react-bulma-components/lib/components/form';
+import Button from 'react-bulma-components/lib/components/button';
+import Icon from 'react-bulma-components/lib/components/icon';
+
 export default class Search extends Component {
+  
   render() {
+    const { email, name, password, comment, gender, question, termsAccepted } = this.state;
     return (
       <div>
-        <h1>Welcome to the search Page</h1>
+        <Field>
+              <Label>Gender</Label>
+              <Control>
+                <Select onChange={this.onChange} name="gender" value={gender}>
+                  <option value="">Select</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other?</option>
+                </Select>
+              </Control>
+            </Field>
       </div>
     )
   }
 }
+
