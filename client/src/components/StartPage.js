46b8038aa7c455d3dd06ea9a9e49b1928e0950ca
mnib2xu <b.r.wakker@gmail.com>
2019-03-21 15:42:17 +0100

    fixed homepage

--- client/src/components/StartPage.js
+++ client/src/components/StartPage.js
@@ -50,7 +50,7 @@ function Explanation() {
         <h1 className="title">
           Affordable solo travels in three steps
         </h1>
-        <p className="subtitle is-size-4" style={{marginBottom: "21px"}}>
+        <p className="subtitle is-size-5" style={{marginBottom: "21px"}}>
           Adventshare is for solo outdoorsport travelers who want to rent appartments without paying for empty beds. 
         </p>
         </div>
@@ -58,48 +58,48 @@ function Explanation() {
 
 
           <div className="column">
-          <div className="card area-card" style={{backgroundImage: 'url("/img/rowan-heuvel-21529-unsplash.jpg")'}}>
-          <div className="card-content area-card-content">
-            <div className="content is-flex home-trinity">
-            <figure className="image is-48x48 is-flex" style={{alignItems: 'center', margin: '10px'}}>
-              <img src="/icons/tick.png" alt="" style={{minWidth: '35px'}}/>
-            </figure>
-              <p className="subtitle is-size-5">
-              Select the appartments you are interested in (select multiple)
-              </p>
-            </div>
+          <div className="card step-card" style={{backgroundImage: 'url("/img/rowan-heuvel-21529-unsplash.jpg")'}}>
+          <div className="card-content step-card-content">
+            {/* <div className="content is-flex"> */}
+              <h1 className="title is-size-1 has-text-light has-text-weight-bold has-text-justified">
+              Step One
+              </h1>
+            {/* </div> */}
           </div> 
         </div>
+          <p className="subtitle is-size-5" style={{marginTop:'10px' }}>
+                Select the appartments you are interested in (select multiple)
+          </p>
         </div>
 
         <div className="column">
-          <div className="card area-card" style={{backgroundImage: 'url("/img/pablo-merchan-montes-772142-unsplash.jpg")'}}>
-          <div className="card-content area-card-content">
-            <div className="content is-flex home-trinity">
-            <figure className="image is-48x48 is-flex" style={{alignItems: 'center', margin: '10px'}}>
-              <img src="/icons/refresh.png" alt="" style={{minWidth: '35px'}}/>
-            </figure>
-              <p className="subtitle is-size-5">
-              Wait untill enough people have show interest in one of your selections
-              </p>
-            </div>
+          <div className="card step-card" style={{backgroundImage: 'url("/img/pablo-merchan-montes-772142-unsplash.jpg")'}}>
+          <div className="card-content step-card-content">
+            {/* <div className="content is-flex"> */}
+              <h1 className="title is-size-1 has-text-light has-text-weight-bold has-text-justified">
+              Step Two
+              </h1>
+            {/* </div> */}
           </div> 
         </div>
+          <p className="subtitle is-size-5" style={{marginTop:'10px' }}>
+          Wait untill enough people have show interest in one of your selections
+          </p>
         </div>
 
         <div className="column">
-          <div className="card area-card" style={{backgroundImage: 'url("/img/david-calderon-1065248-unsplash.jpg")'}}>
-          <div className="card-content area-card-content has-shaddow">
-            <div className="content is-flex home-trinity">
-            <figure className="image is-48x48 is-flex" style={{alignItems: 'center', margin: '10px'}}>
-              <img src="/icons/thumb-up.svg" alt="" style={{minWidth: '35px'}}/>
-            </figure>
-              <p className="subtitle is-size-5">
-              Give a final go and start your adventure. Its gonna be sick!
-              </p>
-            </div>
+          <div className="card step-card" style={{backgroundImage: 'url("/img/david-calderon-1065248-unsplash.jpg")'}}>
+          <div className="card-content step-card-content">
+            {/* <div className="content is-flex"> */}
+              <h1 className="title is-size-1 has-text-light has-text-weight-bold has-text-justified">
+              Step Three
+              </h1>
+            {/* </div> */}
           </div> 
         </div>
+          <p className="subtitle is-size-5" style={{marginTop:'10px' }}>
+          Give a final go and start your adventure. Its gonna be sick!
+          </p>
         </div>
 
         </div>
